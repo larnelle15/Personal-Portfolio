b638b66a4384e744bd71bb2e60a9773b53e40de7
Larnelle15 <larnelleankunda50@gmail.com>
2024-08-28 23:44:02 -0400

    Dynamically inputting the application's website files

---
+++ Portfolio/scripts.js
@@ -0,0 +1,14 @@
+function toggleMobileMenu(){
+	document.getElementById("menu").classList.toggle("active");
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+// 	const footer = document.createElement("footer");
+// 	footer.classList.add("footer");
+// 	footer.innerHTML = `
+// 		<div class="footer-container">
+// 			<p>&copy; ${new Date().getFullYear()} Larnelle Ankunda. All rights reserved.</p>
+// 		</div>
+// 	`;
+// 	document.body.appendChild(footer);
+// });

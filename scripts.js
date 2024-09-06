61c3610c53a394dd028eb1bbaa814be5594c1f2d
Larnelle15 <larnelleankunda50@gmail.com>
2024-09-06 11:40:18 -0400

    Adjusted the view for smaller screens like phones

--- scripts.js
+++ scripts.js
@@ -2,13 +2,8 @@ function toggleMobileMenu(){
 	document.getElementById("menu").classList.toggle("active");
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-// 	const footer = document.createElement("footer");
-// 	footer.classList.add("footer");
-// 	footer.innerHTML = `
-// 		<div class="footer-container">
-// 			<p>&copy; ${new Date().getFullYear()} Larnelle Ankunda. All rights reserved.</p>
-// 		</div>
-// 	`;
-// 	document.body.appendChild(footer);
-// });
+function toggleMobileMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+}
+
